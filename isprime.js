1d0dd6cca63e7f90fec74d9f246217781cb03964
Suryakant Sahu <suryakantsahu7879@gmail.com>
2024-08-23 14:02:29 +0530

    prime code added

---
+++ isprime.js
@@ -0,0 +1,17 @@
+function check_prime() {
+    if ((num%2)===0||(num%3)===0) {
+        console.log(`number is not a prime number:${num}`);
+    }    
+    else{
+        console.log(`number is prime number:${num}`);
+    }  
+ }
+    
+num=21;
+check_prime();
+
+
+
+
+
+
