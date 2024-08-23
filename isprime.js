a052c8c7e6cc672166ccf5c70dcb69a3465ebd44
Suryakant7879 <suryakantsahu7879@gmail.com>
2024-08-23 14:46:50 +0530

    added

--- isprime.js
+++ isprime.js
@@ -1,13 +1,16 @@
 function check_prime() {
-    if ((num%2)===0||(num%3)===0) {
+    if  (num===2||num===3){
+        console.log(`number is prime number:${num}`);
+    } 
+    else if ((num%2)===0||(num%3)===0) {
         console.log(`number is not a prime number:${num}`);
-    }    
+    }   
     else{
         console.log(`number is prime number:${num}`);
     }  
  }
     
-num=7;
+num=21;
 check_prime();
 
 
